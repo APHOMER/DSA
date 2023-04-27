4130254c4620d73b9204124808deeb01c5eb49c8
Aphomer <aaphomer@gmail.com>
2023-04-27 17:36:07 +0100

    .......

--- array.js
+++ array.js
@@ -7,6 +7,7 @@ class MyArray {
     get(index) {
         return this.data[index];
     }
+    
 
     push(item) {
         this.data[this.length] = item;
@@ -14,7 +15,7 @@ class MyArray {
         return this.length;
     }
 
-    
+
     pop() {
         const lastItem = this.data[this.length-1];
         delete this.data[this.length-1];
